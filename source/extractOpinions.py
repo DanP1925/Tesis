e29de5a2cbd0b7a0b85fc5393be1984839d92a82
DanP1925 <a20110645@pucp.pe>
2017-02-08 16:17:35 -0500

    extractEntity implemented and test cases covered

--- source/extractOpinions.py
+++ source/extractOpinions.py
@@ -1,8 +1,12 @@
 from xml.etree import cElementTree as ET
 
-def extractEntity(tweet)
-	for sentiments in tweet:
-		sentiment.get('entity')
+def extractEntity(tweet):
+	result = []
+	for sentiment in tweet:
+		entity = sentiment.get('entity')
+		if entity not in result:
+			result.append(entity)
+	return result
 
 def main():
 	doc = "D:\Ciclo 6\Tesis 2\stompol-tweets-train-tagged.xml"
@@ -11,6 +15,7 @@ def main():
 	i=1
 	for tweet in tweets:
 		entities = extractEntity(tweet)
+		print(entities)
 	
 	
 if __name__ == "__main__":
--- source/test/entityExtractionTest.py
+++ source/test/entityExtractionTest.py
@@ -1,5 +1,9 @@
 import unittest
 from xml.etree import cElementTree as ET
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
+import extractOpinions as EO
 
 class EntityExtractionMethod(unittest.TestCase):
 	
@@ -8,10 +12,11 @@ class EntityExtractionMethod(unittest.TestCase):
 	tweets = tree.getroot()
 	
 	def testSingleEntityExtraction(self):
-		self.assertEqual(extractEntity(tweets[0]),['Partido_Popular'])
+		self.assertEqual(EO.extractEntity(self.tweets[0]),['Partido_Popular'])
 		
 	def testMultipleEntityExtraction(self):
-		self.assertEqual(extractEntity(tweets[1]),['Partido_Socialista_Obrero_Espanol','Partido_Popular'])
+		self.assertEqual(EO.extractEntity(self.tweets[1]),['Partido_Socialista_Obrero_Espanol','Partido_Popular'])
 
 if __name__ == '__main__':
-	unittest.main()
+	suite = unittest.TestLoader().loadTestsFromTestCase(EntityExtractionMethod)
+	unittest.TextTestRunner(verbosity=2).run(suite)
