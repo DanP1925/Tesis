3ade5bb2dd4e4dd64fcd0769dfc6980be2caa955
DanP1925 <a20110645@pucp.pe>
2017-07-04 17:59:19 -0500

    Changed Indentation on extractOpinions.py

--- source/extractOpinions.py
+++ source/extractOpinions.py
@@ -5,26 +5,28 @@ import sentiStrength as SENSTR
 import time
 
 def main():
-	#doc = "/home/daniel/data/Ciclo6/Tesis2/stompol-tweets-train-tagged.xml"
-	doc = "/home/daniel/data/Ciclo6/Tesis2/xmlSampleFile.xml"
-	#doc = "/home/daniel/data/Ciclo6/Tesis2/xmlSampleFile2.xml"
-	#doc = "/home/daniel/data/Ciclo6/Tesis2/xmlStandardFile.xml"
+    
+    #doc = "/home/daniel/data/Ciclo6/Tesis2/stompol-tweets-train-tagged.xml"
+    doc = "/home/daniel/data/Ciclo6/Tesis2/xmlSampleFile.xml"
+    #doc = "/home/daniel/data/Ciclo6/Tesis2/xmlSampleFile2.xml"
+    #doc = "/home/daniel/data/Ciclo6/Tesis2/xmlStandardFile.xml"
 	
-	xmlparser = XML.XmlParser(doc)
-	tweets = xmlparser.root
-	corpus = COR.Corpus()
+    xmlparser = XML.XmlParser(doc)
+    tweets = xmlparser.root
+    corpus = COR.Corpus()
 	
-	for tweet in tweets:
-		tweetEntities = xmlparser.extractEntity(tweet)
-		corpus.addNewEntities(tweetEntities)
-		for tweetEntity in tweetEntities:
-			entity = corpus.getEntity(tweetEntity)
-			entity.addReview(tweet)
+    for tweet in tweets:
+        tweetEntities = xmlparser.extractEntity(tweet)
+        corpus.addNewEntities(tweetEntities)
+        for tweetEntity in tweetEntities:
+            entity = corpus.getEntity(tweetEntity)
+            entity.addReview(tweet)
 	
-	for entity in corpus.entities:
-		for review in entity.reviews:
-			print(review.text)
-		print()
+    for entity in corpus.entities:
+        print(entity.name)
+        for review in entity.reviews:
+            print(review.text)
+        print()
 
 if __name__ == "__main__":
 	main()
